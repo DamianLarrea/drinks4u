b0a14155e872623717a4eba091f0e11e3dbecb1a
DamianLarrea <damiantlarrea@gmail.com>
2023-09-09 18:25:32 +1000

    Add new product service

---
+++ API/Core/Products/ProductService.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Products
+{
+    public interface IProductService
+    {
+        IReadOnlyCollection<Product> GetProducts();
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IReadOnlyCollection<Product> GetProducts() => productRepository.GetAll();
+    }
+}
